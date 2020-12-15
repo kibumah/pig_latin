19a4d07fb8a36a7954ed63a0574e9c9445fe8a35
thebriankwon <thebriankwon@gmail.com>
2020-12-15 01:31:51 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Take a string, split into words
+
+// Words that start with a vowel or vowel sounds
+
+// Words that start with a consonant or consonant cluster
